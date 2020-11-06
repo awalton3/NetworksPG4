fcf6b891f61ab1b68f7bb390c2d1b358db46e4ac
ccolon2 <ccolon2@nd.edu>
2020-11-05 22:04:52 -0500

    add basic stuff

--- program4/pong_cpp/pong.cpp
+++ program4/pong_cpp/pong.cpp
@@ -5,11 +5,108 @@
 #include <string.h>
 #include <sys/time.h>
 
+
+#include <iostream>
+#include <string>
+
+#include <strings.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fstream>
+#include <time.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <map>
+
+//#include <string>
+
+#define MAX_SIZE 4096
+#define MAX_PENDING 5
+
+using namespace std;
+
 #define WIDTH 43
 #define HEIGHT 21
 #define PADLX 1
 #define PADRX WIDTH - 2
+//Logging 
+void printLog(char *message){
+    FILE *f = fopen("log", "a");
+    fprintf(f,"%s\n",message);
+    fclose(f);
+}
 
+int connectHost(int port){
+    //Networking code
+    struct sockaddr_in sock;
+    // Clear memory location
+    bzero((char*) &sock, sizeof(sock));
+    // Specify IPv4
+    sock.sin_family = AF_INET;
+    // Use the default IP address of the server
+    sock.sin_addr.s_addr = INADDR_ANY;
+    // Convert from host to network byte order
+    sock.sin_port = htons(port);
+
+    /* Set up socket */
+    int sockfd;
+    
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket failed.");
+        return 1;
+    }
+
+    /* Set socket options */
+    int opt = 1;
+    if ((setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,(char*)&opt, sizeof(int))) < 0) {
+        perror("Setting socket options failed.");
+        return 1;
+    }
+    /* Bind */ 
+    if ((bind(sockfd, (struct sockaddr*) &sock, sizeof(sock))) < 0) {
+        perror("Bind failed.");
+        return 1;
+    }
+
+    /* Listen */
+    if ((listen(sockfd, MAX_PENDING)) < 0) {
+        perror("Listen failed.");
+        return 1; 
+    } 
+
+    /* Wait for incoming connections */
+    int new_sockfd;
+    struct sockaddr_in client_sock;
+    socklen_t len = sizeof(client_sock);
+    pthread_t thread_id;
+    
+    cout << "Wait for connection..." << endl;
+    while (1) {
+
+        new_sockfd = accept(sockfd, (struct sockaddr*) &client_sock, &len);
+             
+        if (new_sockfd < 0) {
+                        cout << "exiting " << endl; 
+            perror("Accept failed.");
+                        cout << "exiting " << endl; 
+            return 1;
+        }
+
+      
+        /*if(pthread_create(&thread_id, NULL, connection_handler, (void*) &new_sockfd) < 0) {
+            perror("could not create thread");
+            return 1;
+        }*/
+
+    }
+        
+}
 // Global variables recording the state of the game
 // Position of ball
 int ballX, ballY;
@@ -19,8 +116,8 @@ int dx, dy;
 int padLY, padRY;
 // Player scores
 int scoreL, scoreR;
-
 // ncurses window
+
 WINDOW *win;
 
 /* Draw the current game state to the screen
@@ -173,8 +270,26 @@ int main(int argc, char *argv[]) {
     // Process args
     // refresh is clock rate in microseconds
     // This corresponds to the movement speed of the ball
-    int refresh;
-    char difficulty[10]; 
+    
+    int  refresh;
+    char difficulty[10];
+    if(argc != 3){
+        printf("Not enough arguments.");
+        return 1;
+
+    }
+
+    int    port = stoi(argv[2]);
+    string host = argv[1];
+    if(host == "--host" ){
+        //cout << "i am the server" << endl;
+        printLog("i am the server");
+        
+        connectHost(port);
+        
+    }
+   
+    ///////////////////
     printf("Please select the difficulty level (easy, medium or hard): ");
     scanf("%s", &difficulty);
     if(strcmp(difficulty, "easy") == 0) refresh = 80000;
@@ -212,3 +327,7 @@ int main(int argc, char *argv[]) {
     endwin();
     return 0;
 }
+
+
+
+
